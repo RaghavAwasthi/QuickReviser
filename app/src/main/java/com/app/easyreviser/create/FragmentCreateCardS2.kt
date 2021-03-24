047af0f30a7fd11f7cef66682e5f7104e815ede0
Raghav Awasthi <raghavawasthi2014@gmail.com>
2021-03-25 04:09:07 +0530

    Created S1,S2,S3 mocks

---
+++ app/src/main/java/com/app/easyreviser/create/FragmentCreateCardS2.kt
@@ -0,0 +1,19 @@
+package com.app.easyreviser.create
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentCreateCardS2 : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
