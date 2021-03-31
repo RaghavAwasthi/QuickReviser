c1c9fa4aef7b93cb177c4101018b9c1b0d4b9b58
Raghav Awasthi <raghavawasthi2014@gmail.com>
2021-03-31 10:02:25 +0530

    Improved Landing Intro Screens

--- app/src/main/java/com/app/easyreviser/init/IntroActivity.kt
+++ app/src/main/java/com/app/easyreviser/init/IntroActivity.kt
@@ -3,54 +3,53 @@ package com.app.easyreviser.init
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.app.easyreviser.R
 import com.app.easyreviser.repository.AppPreference
 import com.app.easyreviser.repository.Constants
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroFragment
-import com.github.appintro.AppIntroPageTransformerType
 
 class IntroActivity : AppIntro() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addSlide(
-            AppIntroFragment.newInstance(
-                title = "Welcome to Easy Revisor", titleColor = Color.WHITE,
-                description = "App Let's you revise stuff periodically",
-                backgroundColor = Color.argb(8, 240, 220, 220),
-            )
-        )
-        addSlide(
-            AppIntroFragment.newInstance(
-                title = "Create Flash Cards",
-                titleColor = Color.BLACK,
-                descriptionColor = Color.BLACK,
-                description = "Helps in Quick Revision",
-                backgroundColor = Color.WHITE,
-            )
-        )
-
-        addSlide(
-            AppIntroFragment.newInstance(
-                title = "Wooaah hoo!!",
-                titleColor = Color.WHITE,
-                descriptionColor = Color.WHITE,
-                description = "Let those cards remind periodically",
-                backgroundColor = Color.YELLOW,
-            )
-        )
-
-        setProgressIndicator()
         isVibrate = true
         vibrateDuration = 50L
         isWizardMode = true
         isColorTransitionsEnabled = true
-        setTransformer(
-            AppIntroPageTransformerType.Parallax(
-                titleParallaxFactor = 1.0,
-                imageParallaxFactor = -1.0,
-                descriptionParallaxFactor = 2.0
-            )
+        addSlide(
+                AppIntroFragment.newInstance(
+                        title = "Welcome to Easy Revisor", titleColor = Color.WHITE,
+                        description = "App Let's you revise stuff periodically",
+                        backgroundColor = Color.parseColor("#2196f3"),
+                        imageDrawable = R.drawable.intro_1
+                )
         )
+        addSlide(
+                AppIntroFragment.newInstance(
+                        title = "Create Flash Cards",
+                        titleColor = Color.WHITE,
+                        descriptionColor = Color.WHITE,
+                        description = "Helps in Quick Revision",
+                        backgroundColor = Color.parseColor("#009688"),
+                        imageDrawable = R.drawable.ic_intro_2
+
+                )
+        )
+
+        addSlide(
+                AppIntroFragment.newInstance(
+                        title = "Wooaah hoo!!",
+                        titleColor = Color.WHITE,
+                        descriptionColor = Color.WHITE,
+                        description = "Let those cards remind periodically",
+                        backgroundColor = Color.parseColor("#43a047"),
+                        imageDrawable = R.drawable.intro_3
+                )
+        )
+
+        setProgressIndicator()
+
+
     }
 
     override fun onSkipPressed(currentFragment: Fragment?) {
